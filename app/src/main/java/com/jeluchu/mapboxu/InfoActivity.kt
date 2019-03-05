eedd62cda81b5bb6eb6007bc47b95f8284d23abf
Jeluchu <infojeluchu@gmail.com>
2019-03-05 16:55:36 +0100

    LITTLE FIXES

--- app/src/main/java/com/jeluchu/mapboxu/InfoActivity.kt
+++ app/src/main/java/com/jeluchu/mapboxu/InfoActivity.kt
@@ -1,11 +1,17 @@
 package com.jeluchu.mapboxu
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.toolbar.*
 
 
 class InfoActivity : AppCompatActivity() {
+
+    private lateinit var menuView: Menu
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,4 +24,23 @@ class InfoActivity : AppCompatActivity() {
 
         toolbar.setNavigationOnClickListener { finish() }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_info, menu!!)
+        menuView = menu
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when (item!!.itemId) {
+            R.id.gitHub -> {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_github)))
+                startActivity(intent)
+            }
+
+        }
+        return true
+    }
+
 }
